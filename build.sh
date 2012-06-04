176e101704cb8091b361e43760e0dbe8f228ec40
Chad Crabtree <flaxeater@gmail.com>
2012-06-03 21:39:43 -0400

    using a new bash trick I just learned

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dir=$(pwd)
 cd ../..
-javac -deprecation -g -source 1.5 -encoding UTF-8 -classpath /usr/lib/jvm/java-6-sun-1.6.0.26/jre/lib/rt.jar:libs/robocode.jar:/home/ccrabtree/robocode/robots $dir/RadionAtascar.java
+javac -deprecation -g -source 1.5 -encoding UTF-8 -classpath /usr/lib/jvm/java-6-sun-1.6.0.26/jre/lib/rt.jar:libs/robocode.jar:${dir%/*} $dir/RadionAtascar.java
 cd $dir
